0e49295a70bf63a3547daf74609e5fb48474f0b7
Daschuko777 <darinadruz92@gmail.com>
2023-05-20 18:54:40 +0300

    super

---
+++ main.py
@@ -0,0 +1,28 @@
+class Hello:
+    def __init__(self):
+        print("Helllo")
+
+class HelloWorld(Hello):
+    def __init__(self):
+        super().__init__()
+        print("World!")
+
+hello_word = HelloWorld()
+
+class Computer:
+    def __init__(self):
+        super().__init__()
+        self.memory = 128
+
+class Display:
+    def __init__(self):
+        super().__init__()
+        self.resolution = "4k"
+
+class SmartPhone(Display, Computer):
+    def print_info(self):
+        print(self.resolution)
+        print(self.memory)
+
+iphone = SmartPhone()
+iphone.print_info()
